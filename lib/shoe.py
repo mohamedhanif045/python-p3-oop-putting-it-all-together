8800e2c4927e80fb2404a81bcf006dd80913109e
mohamedhanif045 <hanifmohamed045@gmail.com>
2024-08-22 10:01:07 +0300

    initial commit

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
-#!/usr/bin/env python3
+# lib/shoe.py
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "Used"  # Initialize condition
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        elif value <= 0:
+            raise ValueError("size must be a positive number")
+        else:
+            self._size = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"  # Set condition to "New" after cobbling
